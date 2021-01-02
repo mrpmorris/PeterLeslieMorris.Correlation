d823b345e691d1ebf9f68e07befb1eb960f48c60
Peter Morris <mrpmorris@gmail.com>
2021-01-02 13:46:03 +0000

    Initial check-in

---
+++ PeterLeslieMorris.Correlation/CorrelationId.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+
+namespace PeterLeslieMorris.Correlation
+{
+	public static class CorrelationId
+	{
+		private readonly static AsyncLocal<string> AsyncLocalValue = new AsyncLocal<string>();
+
+		public static string Value
+		{
+			get => AsyncLocalValue.Value;
+			set
+			{
+				string existingValue = Value;
+				if (existingValue != null && existingValue != value)
+					throw new CorrelationIdAlreadySetException(existingValue: existingValue, newValue: value);
+				AsyncLocalValue.Value = value;
+			}
+		}
+	}
+}
---
+++ PeterLeslieMorris.Correlation/CorrelationIdAlreadySetException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PeterLeslieMorris.Correlation
+{
+	public class CorrelationIdAlreadySetException : Exception
+	{
+		public string ExistingValue { get; }
+		public string NewValue { get; }
+
+		public CorrelationIdAlreadySetException(string existingValue, string newValue)
+			: base($"Cannot change {nameof(CorrelationId)}.{nameof(CorrelationId.Value)} from '{existingValue}' to '{newValue}'")
+		{
+			ExistingValue = existingValue;
+			NewValue = newValue;
+		}
+	}
+}
