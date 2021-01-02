88637445433b22a30fd1a277835c671396efd496
Peter Morris <mrpmorris@gmail.com>
2021-01-02 16:44:24 +0000

    Rename property to Pipeline

--- Source/PeterLeslieMorris.Correlation/CorrelationId.cs
+++ Source/PeterLeslieMorris.Correlation/CorrelationId.cs
@@ -4,7 +4,7 @@ namespace PeterLeslieMorris.Correlation
 {
 	public sealed class CorrelationId
 	{
-		public static CorrelationId Populate { get; }
+		public static CorrelationId Pipeline { get; }
 		private readonly static AsyncLocal<string> AsyncLocalValue = new AsyncLocal<string>();
 
 		private CorrelationId() { }
